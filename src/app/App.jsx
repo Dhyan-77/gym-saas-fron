f53f1fc3b183eee28eced1d79c72447dc98f39ea
Dhyan V Shetty <dhyanshetty777@gmail.com>
2026-02-23 12:45:54 +0530

    eee

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,6 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 
 export default function App() {
-  useEffect(() => {
-  const token = localStorage.getItem("access");
-  if (token) {
-    // optionally verify token or fetch user
-  }
-}, []);
+  
   return <RouterProvider router={router} />;
 }
